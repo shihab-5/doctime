df7280109a92b2edd1f40e5501937f64c19197df
shihab-5 <shihabuli364@gmail.com>
2026-06-13 22:33:42 +0600

    verce deploy 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,48 @@
 /** @type {import('next').NextConfig} */
+
+/** @type {import('next').Next} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
   experimental: {
-    serverActions: true,
+    serverComponentsExternalPackages: ['@better-auth/kysely-adapter', 'kysely'],
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
+        port: '',
+        pathname: '**',
+        search: '',
       },
-      
-      
-      
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
     ],
-
   },
-  
-  reactCompiler: true,
 };
 
 export default nextConfig;
+// const nextConfig = {
+//   /* config options here */
+//   experimental: {
+//     serverActions: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '**',
+//       },
+      
+      
+      
+//     ],
+
+//   },
+  
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
